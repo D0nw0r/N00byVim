5a2058e447d53ce23bbe2b00d7111c19e08918a8
DCardosoGH <diogo.g.c@gmail.com>
2025-08-08 09:49:19 +0200

    insert mode exit via jk jj

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', { desc = 'New tab' })
 vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next buffer' })
 vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Previous buffer' })
 vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Delete buffer' })
+
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = false })
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = false })
