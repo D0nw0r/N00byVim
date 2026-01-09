fb9cd815e1ac82b4e9578845a37a524160acd63c
D0nw0r <diogo.g.c@gmail.com>
2026-01-09 15:26:19 +0100

    folding

--- lua/custom/opts/init.lua
+++ lua/custom/opts/init.lua
@@ -1,1 +1,9 @@
 vim.opt.shiftwidth = 4
+
+-- Folding configuration with Treesitter
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 99 -- Start with all folds open
+vim.opt.foldlevelstart = 99 -- Open all folds when opening file
+vim.opt.foldenable = true
+vim.opt.foldcolumn = '1' -- Show fold column (optional, adds visual indicator)
